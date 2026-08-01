4fc0a464ec7fad24afd4cad3684a17716d54082f
arwainme <arwaaboganba8@gmail.com>
2026-08-01 12:32:16 +0300

    Day5-2

---
+++ RasElbarMVCDayFour/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RasElbarMVCDayFour.Models;
+
+namespace RasElbarMVCDayFour.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        CompanyDbContext _context = new CompanyDbContext();
+
+        public IActionResult GetAll()
+        {
+            var emps = _context.Employees.ToList();
+
+            return View("GetAll",emps);
+        }
+        public IActionResult Add()
+        {
+            ViewData
+            return View("Add");
+        }
+
+        public IActionResult SaveAdd(Employee empFromUser)
+        {
+
+            if (ModelState.IsValid) 
+            { 
+                _context.Employees.Add(empFromUser);
+                _context.SaveChanges();
+
+                return RedirectToAction("GetAll");
+
+
+            }
+         
+            return View("Add");
+            
+
+        }
+    }
+}
--- RasElbarMVCDayFour/Models/Employee.cs
+++ RasElbarMVCDayFour/Models/Employee.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RasElbarMVCDayFour.Models
 {
@@ -6,6 +7,7 @@ namespace RasElbarMVCDayFour.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Full Name")]
         public string Name { get; set; }
 
         public string Address { get; set; }
