c73cc498862f2a084812b86d67d9cf0a9aa06e71
arwainme <arwaaboganba8@gmail.com>
2026-08-01 10:56:03 +0300

    day 5

--- RasElbarMVCDayFour/Models/Department.cs
+++ RasElbarMVCDayFour/Models/Department.cs
@@ -1,10 +1,12 @@
-﻿namespace RasElbarMVCDayFour.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RasElbarMVCDayFour.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        //[Display(Name = "Marwan")]
         public string MangerName { get; set; }
 
         public List<Employee>? Employees { get; set; } = new List<Employee>();
